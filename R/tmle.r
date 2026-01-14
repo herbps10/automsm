de89a1c573c90df87c48e9f6a7376759731066c2
Herb Susmann <herbps10@gmail.com>
2026-01-14 14:07:45 -0500

    Updated for check

--- R/tmle.r
+++ R/tmle.r
@@ -4,7 +4,7 @@ tmle_mle <- function(p, fluctuation_model, ...) {
   epsilon <- torch::torch_tensor(rep(0, p), requires_grad = TRUE)
   optimizer <- torch::optim_lbfgs(epsilon)
   for(iter in 1:2) {
-    optimizer$step(\() {
+    optimizer$step(function() {
       optimizer$zero_grad()
       target <- fluctuation_model(epsilon, ...)
       #cat(glue::glue("TMLE iteration {iter} target: {as.numeric(target)}\n\n"))
