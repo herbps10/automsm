209201c45c44c1d815d3c46b920f222b71ca4184
Herb Susmann <herbps10@gmail.com>
2026-08-13 12:01:03 -0400

    Added helpers

--- R/helpers.r
+++ R/helpers.r
@@ -1,3 +1,5 @@
+`%||%` <- function(x, y) if(is.null(x)) y else x
+
 bound <- function(x, min, max, epsilon) {
   x[x <= min + epsilon] <- epsilon
   x[x >= max - epsilon] <- epsilon
@@ -6,4 +8,11 @@ bound <- function(x, min, max, epsilon) {
 
 bound01 <- function(x, epsilon) {
   bound(x, 0, 1, epsilon)
+}
+
+#' Coerce to a float32 torch tensor, preserving shape
+#' @noRd
+as_float_tensor <- function(x) {
+  if(inherits(x, "torch_tensor")) return(x$to(dtype = torch::torch_float()))
+  torch::torch_tensor(x, dtype = torch::torch_float())
 }
